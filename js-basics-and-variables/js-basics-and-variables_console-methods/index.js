0466e128d40a89b938fa73afbd1896401a99afc8
Hannah Capell <hannah.capell@gmail.com>
2025-12-12 15:45:45 +0100

    finish tasks in console methods

--- js-basics-and-variables/js-basics-and-variables_console-methods/index.js
+++ js-basics-and-variables/js-basics-and-variables_console-methods/index.js
@@ -7,15 +7,22 @@ console.clear();
 // 1. Store the sentence 'Happy coding!' in a variable named 'coding'.
 
 // 2. Display the variable 'coding' as a message in the console.
+coding = "happy Coding";
+console.log(coding);
 
 // 3. Store the sentence 'Happy hacking!' in a variable named 'hacking'.
 
 // 4. Display the variable 'hacking' as a message in the console.
+hacking = "happy hacking";
+console.log(hacking);
 
 // 5. Which method is used to clear the console? Use it to tidy up the console.
-
+console.clear();
 // 6. Store the text 'This is an error!' in a variable 'error'.
+error = "This is an error";
+console.log(error);
 
 // 7. Which method is used to display an error message in the console? Use it do display the error message in the console.
+console.error(error);
 
 // Well done!
