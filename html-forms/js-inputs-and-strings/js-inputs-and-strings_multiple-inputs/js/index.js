c8dc6b7f66fa8294a8bbbc1158904620a592d304
Hannah Capell <hannah.capell@gmail.com>
2025-12-17 16:25:20 +0100

    Task1: copy, from multiple-inputs-challenge

--- html-forms/js-inputs-and-strings/js-inputs-and-strings_multiple-inputs/js/index.js
+++ html-forms/js-inputs-and-strings/js-inputs-and-strings_multiple-inputs/js/index.js
@@ -5,7 +5,9 @@ const secondCopyInput = document.querySelector("[data-js=second-copy-input]");
 const copyButton = document.querySelector("[data-js=copy-button]");
 
 // --v-- Write your code here --v--
-
+copyButton.addEventListener("click", () => {
+  secondCopyInput.value = firstCopyInput.value;
+});
 // --^-- End of Task 1 --^--
 
 // Task 2: Copy and Uppercase
