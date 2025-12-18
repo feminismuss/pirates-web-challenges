fded2bde038f069a6cbbf30213960288fd5bfaa3
Hannah Capell <hannah.capell@gmail.com>
2025-12-18 16:08:58 +0100

    finish toastmessages challenges

--- js-createElement/js-createelement_toast-messages/js/index.js
+++ js-createElement/js-createelement_toast-messages/js/index.js
@@ -4,10 +4,17 @@ const toastContainer = document.querySelector('[data-js="toast-container"]');
 const addButton = document.querySelector('[data-js="add-button"]');
 const clearButton = document.querySelector('[data-js="clear-button"]');
 
-addButton.addEventListener("click", () => {
+addButton.addEventListener("click", (event) => {
+  event.preventDefault();
+  const li = document.createElement("li");
+  toastContainer.append(li);
+  li.textContent = "another toastmessage";
+  li.classList.add("toast-container__message");
   // Exercise: Append a new entry to the toast messages container
 });
 
-clearButton.addEventListener("click", () => {
-  // Exercise: Clear the stack of toast messages
+clearButton.addEventListener("click", (event) => {
+event.preventDefault();
+toastContainer.innerHTML = "";
+// Exercise: Clear the stack of toast messages
 });
