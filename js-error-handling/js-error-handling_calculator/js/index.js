c574c7d78b2f2293f36e70cf2bd900c8d1c266ae
Hannah Capell <hannah.capell@gmail.com>
2026-01-09 11:56:58 +0100

    add notes for better understanding

--- js-error-handling/js-error-handling_calculator/js/index.js
+++ js-error-handling/js-error-handling_calculator/js/index.js
@@ -7,7 +7,8 @@ const operations = {
   add: (a, b) => a + b,
   subtract: (a, b) => a - b,
   multiply: (a, b) => a * b,
-  divide: (a, b) => {
+  // hier steht die Anleitung fuers teilen samt Bedingung, und Fehlermeldung
+  divide: (a, b) => { 
     if (b === 0) {
       throw new Error("Cannot divide by zero!");
     }
@@ -20,9 +21,12 @@ form.addEventListener("submit", (event) => {
   const firstNumber = Number(event.target.firstNumber.value);
   const secondNumber = Number(event.target.secondNumber.value);
   const operation = event.target.operation.value;
+  // hier muss die Fehlermeldung eingefuegt werden, weil hier die Berechnung auf "click" stattfindet 
   try {
     output.innerText = operations[operation](firstNumber, secondNumber);
-  } catch (error) {
+  } 
+  // der catch muss den error als parameter definiert bekommen, sonst kannt er ihn nicht anzeigen
+  catch (error) {
     output.innerText = error.message;
   }
 });
