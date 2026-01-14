67eaa4eb5f3fd63a359986064746f648d64905f7
Hannah Capell <hannah.capell@gmail.com>
2026-01-14 16:02:25 +0100

    map question and ansers as string and oject

--- js-array-methods/js-array-methods_map/js/index.js
+++ js-array-methods/js-array-methods_map/js/index.js
@@ -31,8 +31,15 @@ const lowerCaseAnswers = cards.map((card) => {
   return result;
 }); // ['as often as you like.', ...]
 
-const questionsAndAnswersTogether = null; // ["How often can I use <header>? - As often as you like.", ...]
-
-const questionAndAnswer = null; // [{ question: 'How often can I use <header>?', answer: 'As often as you like.'}, {...}]
+const questionsAndAnswersTogether = cards.map((card) => {
+  return `${card.question} - ${card.answer}` 
+}); // ["How often can I use <header>? - As often as you like.", ...]
+console.log(questionsAndAnswersTogether);
+const questionAndAnswer = cards.map((card) => {
+  return {
+    question: card.question,
+    answer: card.answer
+  }; 
+}); // [{ question: 'How often can I use <header>?', answer: 'As often as you like.'}, {...}]
 
 export { lowerCaseAnswers, questionsAndAnswersTogether, questionAndAnswer };
