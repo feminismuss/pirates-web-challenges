cb71766195be70f9c7363f055fb0d13e894afb51
Hannah Capell <hannah.capell@gmail.com>
2026-01-16 12:41:27 +0100

    finish challenge

--- react-basics/react-basics_attributes/src/App.jsx
+++ react-basics/react-basics_attributes/src/App.jsx
@@ -1,17 +1,18 @@
 import "./styles.css";
 
 export default function App() {
-  return <article>Replace me with an article</article>;
+  return <Article />;
 }
 
 function Article() {
   return (
-  <article className="article">
-    <h2>Attributes in React</h2>
-    <label htmlFor="questionInput">Ask your Question here:</label>
-    <input id="questionInput"type="text" />
-    <a className="article__link" href="https://de.wikipedia.org/wiki/React">
+    <article className="article">
+      <h2>Attributes in React</h2>
+      <label htmlFor="questionInput">Ask your Question here:</label>
+      <input id="questionInput" type="text" />
+      <a className="article__link" href="https://de.wikipedia.org/wiki/React">
         Learn more about React
       </a>
-  </article>)
+    </article>
+  );
 }
