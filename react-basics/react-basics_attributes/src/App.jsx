42047e95c8cc966327df2aa984677a22605d8106
Hannah Capell <hannah.capell@gmail.com>
2026-01-16 12:23:36 +0100

    Initial commit

---
+++ react-basics/react-basics_attributes/src/App.jsx
@@ -0,0 +1,14 @@
+import "./styles.css";
+
+export default function App() {
+  return <article>Replace me with an article</article>;
+}
+
+function Article() {
+  <article className="article">
+    <h2>Attributes in React</h2>
+    <label htmlFor="Question">Ask your Question here:</label>
+    <input type="text" />
+    
+  </article>
+}
