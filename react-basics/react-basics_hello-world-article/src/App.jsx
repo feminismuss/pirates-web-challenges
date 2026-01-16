287797a482f8d1e7da739552e4df636ccbfb29d0
Hannah Capell <hannah.capell@gmail.com>
2026-01-16 09:36:36 +0100

    finish challenge

---
+++ react-basics/react-basics_hello-world-article/src/App.jsx
@@ -0,0 +1,13 @@
+import "./styles.css";
+
+export default function App() {
+  return <HelloWorldArticle />;
+}
+function HelloWorldArticle() {
+  return (
+    <article>
+      <h1>Now its time ...</h1>
+      <p>to explore the world of React. Buckle up and lets start</p>
+    </article>
+  );
+}
