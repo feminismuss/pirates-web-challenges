279db35e2a98e6d5151f69ed0cdbb9be33b2e870
Hannah Capell <hannah.capell@gmail.com>
2026-01-20 09:06:13 +0100

    finish challenges

--- react-arrays/react-with-arrays_fruits/src/App.jsx
+++ react-arrays/react-with-arrays_fruits/src/App.jsx
@@ -1,11 +1,19 @@
 import Card from "./components/Card";
 
 export default function App() {
-  const fruits = [];
+  const fruits = [
+    { id: 1337, name: "🍌 banana", color: "yellow" },
+    { id: 1338, name: "🍎 apple", color: "red" },
+    { id: 1339, name: "🍐 pear", color: "lightgreen" },
+    { id: 1340, name: "🍇 grape", color: "purple" },
+    { id: 1341, name: "plum", color: "violet" },
+  ];
 
   return (
     <div className="app">
-      <Card name="🍌 banana" />
+      {fruits.map((fruit) => (
+        <Card key={fruit.id} name={fruit.name} color={fruit.color} />
+      ))}
     </div>
   );
 }
