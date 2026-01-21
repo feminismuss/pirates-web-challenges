ef27c8eadbb43dd063e62181be3375c983922a37
Hannah Capell <hannah.capell@gmail.com>
2026-01-21 12:55:44 +0100

    finish challenge Favorite Holiday

--- react-state-2/react-state-2_favourite-holiday/src/App.jsx
+++ react-state-2/react-state-2_favourite-holiday/src/App.jsx
@@ -1,8 +1,20 @@
+import { useState } from "react";
 import "./styles.css";
 
 export default function App() {
+  const [holiday, setHoliday] = useState("");
+  const [date, setDate] = useState("");
+
   function handleSubmit(event) {
     event.preventDefault();
+    const formData = new FormData(event.target)
+    const data = Object.fromEntries(formData)
+    // const holidayValue = event.target.elements.holiday.value;
+    // const dateValue = event.target.elements.date.value;
+    setHoliday(data.holiday);
+    setDate(data.date);
+    event.target.reset();
+    event.target.elements.holiday.focus();
   }
 
   return (
@@ -29,10 +41,10 @@ export default function App() {
       </form>
       <h2>Output of Submitted Data</h2>
       <p>
-        Favourite Holiday: <span className="output">New Year</span>
+        Favourite Holiday: <span className="output">{holiday}</span>
       </p>
       <p>
-        Date: <span className="output">Well...</span>
+        Date: <span className="output">{date}</span>
       </p>
     </div>
   );
