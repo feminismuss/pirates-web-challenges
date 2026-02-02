63e97abcdf9d0c7c9402b8b381d85cbe6d28caee
Hannah Capell <hannah.capell@gmail.com>
2026-02-02 09:07:50 +0100

    finish challenge

---
+++ nextjs-dynamic-routes_lotr-app-dynamic-routes/pages/volumes/index.js
@@ -0,0 +1,28 @@
+import Link from "next/link";
+import { introduction, volumes } from "../../lib/data";
+import { useRouter } from "next/router";
+
+function getRandomElement(array) {
+  return array[Math.floor(Math.random() * array.length)];
+}
+export default function Volumes() {
+  const router = useRouter();
+  function handleRandomVolume() {
+    const randomVolume = getRandomElement(volumes);
+    router.push(`/volumes/${randomVolume.slug}`);
+  }
+  return (
+    <>
+      <h1>The Lord of the Rings</h1>
+      <p>{introduction}</p>
+      <ul>
+        {volumes.map((volume) => (
+          <li key={volume.slug}>
+            <Link href={`/volumes/${volume.slug}`}>{volume.title}</Link>
+          </li>
+        ))}
+      </ul>
+      <button onClick={handleRandomVolume}>Random Volume</button>
+    </>
+  );
+}
