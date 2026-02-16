de0bae4a9a388f91e8be3e505a76fb73c3bc9120
Hannah Capell <hannah.capell@gmail.com>
2026-02-16 17:20:46 +0100

    finish challenge random-character

---
+++ backend-api-routes/backend-api-routes_random-character/pages/api/random-character.js
@@ -0,0 +1,12 @@
+import Chance from "chance";
+
+const chance = new Chance();
+export default function handler(request, response) {
+  const character = {
+    firstName: chance.first(),
+    lastName: chance.last(),
+    profession: chance.profession(),
+    geohash: chance.geohash(),
+  };
+  response.status(200).json(character);
+}
