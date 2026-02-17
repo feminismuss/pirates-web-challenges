a4c677d1cee36ad8dcbc8fff3981fcf8f93f1945
Hannah Capell <hannah.capell@gmail.com>
2026-02-17 08:49:03 +0100

    finish challenge products

---
+++ backend/backend-read_products/pages/api/products/[id].js
@@ -0,0 +1,19 @@
+import Product from "@/db/models/Product";
+import dbConnect from "@/db/connect";
+
+export default async function handler(request, response) {
+  await dbConnect();
+
+  const { id } = request.query;
+
+  if (request.method === "GET") {
+    const product = await Product.findById(id);
+
+    if (!product) {
+      return response.status(404).json({ status: "Not Found" });
+    }
+    return response.status(200).json(product);
+  } else {
+    return response.status(405).json({ message: "Method not allowed" });
+  }
+}
